93a2fae40dd1760871bd31c4cc3cbd96bba97f7f
Ülari Ainjärv <ylari.ainjarv@gmail.com>
2026-01-26 12:01:36 +0200

    Added L9110 motor driver

---
+++ l9110/src/main.cpp
@@ -0,0 +1,58 @@
+/*
+ * Arduino Labs - Digipurk, ylari[at]digipurk.ee
+ * L9110 motor driver
+ */
+
+#include <Arduino.h>
+
+const int INA = 5;
+const int INB = 6;
+
+void stopMotor() {
+  analogWrite(INA, 0);
+  analogWrite(INB, 0);
+}
+
+void brakeMotor() {
+  analogWrite(INA, 255);
+  analogWrite(INB, 255);
+}
+
+void setMotor(int speed) {
+  speed = constrain(speed, -255, 255);
+  if (speed > 0) {
+    analogWrite(INA, speed);
+    analogWrite(INB, 0);
+  } else if (speed < 0) {
+    analogWrite(INA, 0);
+    analogWrite(INB, -speed);
+  } else {
+    stopMotor();
+  }
+}
+
+void rampTo(int target, int stepDelayMs = 10) {
+  static int cur = 0;
+  int step = (target > cur) ? 1 : -1;
+  while (cur != target) {
+    cur += step;
+    setMotor(cur);
+    delay(stepDelayMs);
+  }
+}
+
+void setup() {
+  pinMode(INA, OUTPUT);
+  pinMode(INB, OUTPUT);
+  stopMotor();
+}
+void loop() {
+  rampTo(200);
+  delay(1500);
+  rampTo(0);
+  delay(200);
+  rampTo(-200);
+  delay(1500);
+  rampTo(0);
+  delay(800);
+}
